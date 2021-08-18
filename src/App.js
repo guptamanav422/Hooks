f3231569d6146103832ad24eb0cf0cdf92c0a70c
Manav <guptamanav422@gmail.com>
2021-08-18 14:59:19 +0530

    3rd case of useEffect

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import {useState,useEffect} from "react";
 let App=()=>{
   const [count, setCount] = useState(0);
 
+  let [process,setProcess]=useState("running");
   console.log("render was called");
   
+  // case 1 
   // useEffect(() => {
   //   console.log("use Effect was called");
   // },[])
 
-  useEffect(() => {
-    console.log("use Effect case2 was called");
-  })
+  // case 2 
+  // useEffect(() => {
+  //   console.log("use Effect case2 was called");
+  // })
+
+  useEffect(()=>{
+    let arr=process.split("i");
+    console.log(arr);
+  },[process])
 
   return (
     <>
@@ -24,6 +32,11 @@ let App=()=>{
     <button onClick={()=>{
       setCount(count-1);
     }}> -</button>
+
+    <p>{process}</p>
+    <button onClick={()=>{
+      setProcess("stop");
+    }}>Kill Process</button>
     </>
   );
 }
