b172442dccc390811ac1f2f7106c3d341da720f0
Manav <guptamanav422@gmail.com>
2021-08-18 16:52:54 +0530

    firebase-setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAG38hPyEWu4OnxmZamS9o9DDTlY6Ej_4M",
+    authDomain: "test-a2f8c.firebaseapp.com",
+    projectId: "test-a2f8c",
+    storageBucket: "test-a2f8c.appspot.com",
+    messagingSenderId: "1019799189820",
+    appId: "1:1019799189820:web:30ed450db7aa82d7490639"
+  };
+
+  firebase.initializeApp(firebaseConfig)
