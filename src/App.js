307e8f3f190f52c9a13c5de7fcb41e960f5f791c
Manav <guptamanav422@gmail.com>
2021-08-18 15:11:16 +0530

    Clean up function in useEffect

--- src/App.js
+++ src/App.js
@@ -3,23 +3,16 @@ import {useState,useEffect} from "react";
 let App=()=>{
   const [count, setCount] = useState(0);
 
-  let [process,setProcess]=useState("running");
   console.log("render was called");
-  
-  // case 1 
-  // useEffect(() => {
-  //   console.log("use Effect was called");
-  // },[])
-
-  // case 2 
-  // useEffect(() => {
-  //   console.log("use Effect case2 was called");
-  // })
 
   useEffect(()=>{
-    let arr=process.split("i");
-    console.log(arr);
-  },[process])
+    console.log("use Effect case2 was called")
+
+    return ()=>{
+      console.log("clean up function was called");
+    };
+
+  })
 
   return (
     <>
@@ -33,10 +26,6 @@ let App=()=>{
       setCount(count-1);
     }}> -</button>
 
-    <p>{process}</p>
-    <button onClick={()=>{
-      setProcess("stop");
-    }}>Kill Process</button>
     </>
   );
 }
